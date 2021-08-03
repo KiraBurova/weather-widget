81e26565e0a3aaaa3284ddfd38c9b61696482bf0
Kira Burova <burovakira11@gmail.com>
2021-08-03 14:08:46 +0300

    tried https over http on request

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,13 +2,13 @@ import axios from 'axios';
 import { TInitialWeatherReport } from '../types';
 
 export const getWeatherReportByLocation = (latitude: number, longitude: number) => {
-  return axios.get<TInitialWeatherReport>(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${process.env.REACT_APP_API_KEY}&units=metric`);
+  return axios.get<TInitialWeatherReport>(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${process.env.REACT_APP_API_KEY}&units=metric`);
 };
 
 export const getWeatherIconByIconName = (icon: string) => {
-  return `http://openweathermap.org/img/wn/${icon}.png`;
+  return `https://openweathermap.org/img/wn/${icon}.png`;
 };
 
 export const getWeatherReportByCityName = (cityName: string) => {
-  return axios.get<TInitialWeatherReport>(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${process.env.REACT_APP_API_KEY}&units=metric`);
+  return axios.get<TInitialWeatherReport>(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${process.env.REACT_APP_API_KEY}&units=metric`);
 };
